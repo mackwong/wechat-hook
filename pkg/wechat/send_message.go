5cb780df9c5474ce925fa7aef073d53beff264b6
Wang Jun <wangjun3@sensetime.com>
2020-06-24 18:30:41 +0800

    init

--- pkg/wechat/send_message.go
+++ pkg/wechat/send_message.go
@@ -7,6 +7,31 @@ import (
 
 var url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=609a7fae-cba3-4eae-afe4-7159e3fcfada"
 
+type Message struct {
+	Wechat string `json:"wechat"`
+	Type string `json:"type"`
+	Content string `json:"content"`
+	MentionedList []string `json:"mentioned_list"`
+	Data interface{} `json:"data"`
+}
+
+type WechatText struct {
+	Context string `json:"context"`
+	MentionedList []string `json:"mentioned_list"`
+}
+
+type WechatMarkdown struct {
+	Context string `json:"context"`
+}
+
+type WechatMessage struct {
+	MsgType string `json:"msgtype"`
+}
+
+func (m *Message) Send() error{
+
+}
+
 func Send() {
 	d := `{
   "msgtype": "text",
